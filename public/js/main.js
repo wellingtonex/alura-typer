8968e3c7beab396e2ee5a3127d71dc9e2598848c
wellington.siqueira <wellingtonex@gmail.com>
2017-02-22 15:19:28 -0300

    adicionando contator de caracteres e palavras e temporizador

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-console.log('ola mundo');
+var frase = $('.frase').text();
+var numeroPalavras = frase.trim().split(
+    ' ').length
+$('#tamanho-frase').text(numeroPalavras);
+
+var campo  = $('.campo-digitacao')
+campo.on('input', () => {
+    let conteudo = campo.val();
+    if(conteudo) {
+        let quantidadePalavras = conteudo.trim().split(" ").length
+        console.log(quantidadePalavras);
+        $('#contador-palavras').text(quantidadePalavras);
+        $('#contador-caracteres').text(conteudo.trim().length);
+    } else {
+        $('#contador-palavras').text(0);
+        $('#contador-caracteres').text(0);
+    }
+   
+});
+
+let tempoRestante =    $('#tempo-digitacao').text();
+    campo.on('focus',  () => {
+    let setIntervalId = setInterval(() => {
+        tempoRestante--;
+         $('#tempo-digitacao').text(tempoRestante);
+         if(tempoRestante < 1) {
+             campo.attr('disabled', true);
+             clearInterval(setIntervalId);
+         }
+    }, 1000)
+});
