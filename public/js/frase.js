b9d37a02a95eee1e3d5f01c7b0e4c2a67544ce02
wellington.siqueira <wellingtonex@gmail.com>
2017-02-23 16:43:06 -0300

    pesquisando por uma frase em especifico, passando parametros para $.get

--- public/js/frase.js
+++ public/js/frase.js
@@ -15,7 +15,29 @@ $('#botao-frase').click(() => {
         }, 2000);
     }).always(() => {
         $('#spinner').hide();
-    });
-    
+    });    
 });
+
+$('#botao-frase-id').click(() => {
+
+    let fraseId = $('#frase-id').val();
+    let dados = {id:fraseId};
+
+    $('#spinner').show();
+    $.get('http://localhost:3000/frases', dados, (data) => {
+        let frase = $('.frase')        
+        frase.text(data.texto);
+        atualizaTamanhoFrase();
+        atualizaTempoInicial(data.tempo);
+        tempo_inicial = data.tempo;
+        $('#erro').hide();
+    }).fail(() => {
+        $('#erro').toggle();
+        setTimeout(function() {
+            $('#erro').toggle();
+        }, 2000);
+    }).always(() => {
+        $('#spinner').hide();
+    }); 
+})
 
