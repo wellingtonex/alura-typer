264fc2e5434fa3998c3425df1fbf5e76316afe71
wellington.siqueira <wellingtonex@gmail.com>
2017-02-22 23:03:43 -0300

    ajustando estilos

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,7 @@ $(() => {
     inicializaContadores();
     let setIntervalId = inicializaCronometro();
     $('#botao-reiniciar').click(renicializaJogo);
+    inicializaMarcadores();
 });
 
 function atualizaTamanhoFrase(){
@@ -37,6 +38,22 @@ function inicializaContadores() {
     });
 }
 
+function inicializaMarcadores() {
+    var frase = $(".frase").text().trim();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("campo-correto");
+            campo.removeClass("campo-errado");
+        } else {
+            campo.addClass("campo-errado");
+            campo.removeClass("campo-correto");        
+        }
+    });
+}
+
 let tempoRestante =    $('#tempo-digitacao').text();
 
 function inicializaCronometro() {
@@ -46,6 +63,7 @@ function inicializaCronometro() {
             $('#tempo-digitacao').text(tempoRestante);
             if(tempoRestante < 1) {
                 campo.attr('disabled', true);
+                campo.toggleClass("campo-desativado");
                 clearInterval(setIntervalId);
             }
         }, 1000)
@@ -60,4 +78,7 @@ function renicializaJogo()  {
     campo.val('');
     clearInterval(setIntervalId);
     inicializaCronometro();
+    campo.removeClass("campo-desativado");
+    campo.removeClass("campo-correto");
+    campo.removeClass("campo-errado")
 }
